d18629b95db55e873663163de2c731d8d63da6fd
zedonboy <zedonbiz@gmail.com>
2021-03-10 12:30:18 +0100

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <section className="w-full h-full relative bg-pink-100">
-      <audio src={song} autoPlay></audio>
+      <audio src={song} controls autoPlay></audio>
       <nav className="bg-transparent max-h-24 flex w-full">
         <div className="w-full flex-shrink-0 md:w-2/3 bg-black border-r-4 border-l-4 border-b-4 border-solid border-yellow-500 h-16 rounded-b-lg ">
           <ul className="m-0 overflow-x-auto flex normal-style p-4 text-white  w-full">
