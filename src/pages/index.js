71632aefb95a9f74418deb6a741aec0282a20ecf
zedonboy <zedonbiz@gmail.com>
2021-03-27 21:21:37 +0100

    Appreciation

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,18 @@ import song from "../assets/music/2.mp3"
 import SimpCard from "../components/SimpCard"
 const IndexPage = () => (
   <Layout>
-    <audio src={song} autoPlay={true} ></audio>
+    <audio src={song} autoPlay={true}></audio>
     <main className="flex flex-wrap text-purple-900 mt-4">
       <div className="w-full md:w-1/2 flex justify-center">
         <img src={bg} />
       </div>
       <div className="w-full md:w-1/2 p-12">
         <h1 className="text-center font-bold normal-style">
-          EX<i><img className="inline candle" src={candle}/></i>T OF A GREAT WOMAN
+          EX
+          <i>
+            <img className="inline candle" src={candle} />
+          </i>
+          T OF A GREAT WOMAN
         </h1>
         <p className="text-center font-thin text-sm md:text-xl">
           OFFICE - THE CANDLE LIGHTER
@@ -35,15 +39,26 @@ const IndexPage = () => (
           NEE Obiajulu
         </p>
         <p className="text-sm text-center">
-          Who went to be with our lord on Monday 11th January 2021. Please join us to celebrate her unique memories and moments  as follows
+          Who went to be with our lord on Monday 11th January 2021. Please join
+          us to celebrate her unique memories and moments as follows
         </p>
-        <SimpCard heading="Vigil Date" answer="5pm Wednesday 17th March, 2021 by His Lordship Most Rev Dr. Paulinues Ezeokafor Catholic Bishop of Akwa diocese"/>
-        <SimpCard heading="Vigil Venue" answer="chief Francis ilo's compound Ezioko village Oko"/>
 
-        <SimpCard heading="Funeral Date" answer="10am Thursday 18th March, 2021 by His lordship Most Rev Dr. Okpalaeke,catholic Bishop of Ekwulobia Diocese"/>
-        <SimpCard heading="Funeral Mass Venue" answer="St. Peter's catholic church Oko, Orumba North LGA , Anambra State."/>
-        <SimpCard heading="Interment Venue" answer="In her husband's compound"/>
-        
+        <p className="text-4xl text-center">Appreciation</p>
+        <p className="text-base text-center">
+          Dear, we are sincerely elated to note that at this time of our. grief,
+          on the loss of our beloved mother, and wife, that we have good friends
+          whom through their prayers, advices, benevolences both financially and
+          in all other positive ways have shown us immerse support. We are
+          really blessed to be associated with your type of person. We lack
+          words to appreciate your magnanimity. May our good Lord who knows how
+          best to bless, replenish your pocket, bless your family and
+          continually keep you in good health. Amen. The Burial and the
+          accompanying ceremonies were a great success. Once more, we say a very
+          big thank you.
+        </p>
+        <p className="text-2xl text-center">
+        With gratitude, from all of us @ Chief francis ilo 🙇🏼‍🙏
+        </p>
       </div>
     </main>
   </Layout>
